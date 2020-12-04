87337c3a484de239ac323f7ff1f6e6b50a92cc46
RoTour <robin.tourne@ynov.com>
2020-12-04 12:00:40 +0100

    inital commit

---
+++ src/main/java/com/ynov/springvacations/SpringvacationsApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.springvacations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvacationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvacationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/springvacations/SpringvacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.springvacations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvacationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
