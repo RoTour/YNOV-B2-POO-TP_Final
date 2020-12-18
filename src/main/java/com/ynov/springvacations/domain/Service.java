140777cfbc1da01b61cc2427077e677c240d7bb7
RoTour <robin.tourne@ynov.com>
2020-12-18 10:45:45 +0100

    fixed -Error: auto increment not setting correctly

--- src/main/java/com/ynov/springvacations/domain/Service.java
+++ src/main/java/com/ynov/springvacations/domain/Service.java
@@ -3,12 +3,13 @@ package com.ynov.springvacations.domain;
 import javax.persistence.*;
 
 @Entity
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "name")})
+//@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "name")})
 public class Service {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public void setId(Long id) {
@@ -18,5 +19,13 @@ public class Service {
     @Id
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
