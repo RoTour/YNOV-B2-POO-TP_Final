8e4168d91c71b86a2a043fc695f33f10db20d8d7
RoTour <robin.tourne@ynov.com>
2020-12-18 11:35:01 +0100

    add show and edit for service controller

--- src/main/java/com/ynov/springvacations/controller/ServiceController.java
+++ src/main/java/com/ynov/springvacations/controller/ServiceController.java
@@ -2,8 +2,11 @@ package com.ynov.springvacations.controller;
 
 import com.ynov.springvacations.domain.Service;
 import com.ynov.springvacations.service.ServiceService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/service")
@@ -15,9 +18,27 @@ public class ServiceController {
         mServiceService = serviceService;
     }
 
+    @RequestMapping("/")
+    public List<Service> getServices(){
+        return mServiceService.getAll();
+    }
+
     @RequestMapping("/create")
     public void create(Service service){
         mServiceService.create(service);
+    }
+
+    @RequestMapping("/{id}")
+    public Service show(@PathVariable String id) {
+        Long idLong = Long.parseLong(id);
+        return mServiceService.findById(idLong);
+    }
+
+    // If id does not exist, creates new service with provided id
+    @RequestMapping("/{id}/edit")
+    public void edit(@PathVariable String id, Service service) {
+        Long idLong = Long.parseLong(id);
+        mServiceService.update(idLong, service);
     }
 
 }
--- src/main/java/com/ynov/springvacations/service/ServiceService.java
+++ src/main/java/com/ynov/springvacations/service/ServiceService.java
@@ -3,6 +3,8 @@ package com.ynov.springvacations.service;
 import com.ynov.springvacations.repository.ServiceRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ServiceService {
 
@@ -14,5 +16,18 @@ public class ServiceService {
 
     public void create(com.ynov.springvacations.domain.Service service) {
         serviceRepository.save(service);
+    }
+
+    public List<com.ynov.springvacations.domain.Service> getAll() {
+        return serviceRepository.findAll();
+    }
+
+    public com.ynov.springvacations.domain.Service findById(Long id) {
+        return serviceRepository.findById(id).orElse(new com.ynov.springvacations.domain.Service());
+    }
+
+    public void update(Long id, com.ynov.springvacations.domain.Service newValues) {
+        newValues.setId(id);
+        serviceRepository.save(newValues);
     }
 }
